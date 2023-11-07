a41ecff3d23b85c13ad1db6bc440310fd3b12594
Christian de MERONA <75143021+Christian-de-MERONA@users.noreply.github.com>
2023-11-07 23:24:43 +0100

    style and scripts added

---
+++ script.js
@@ -0,0 +1,34 @@
+let game = {
+    resultEl: document.getElementById('result'),
+    btnEl: document.getElementById('button'),
+    selectedNumbers: [],
+    init: function(){
+        game.btnEl.addEventListener('click', game.generateNumber);
+        console.log('init');
+    },
+    generateNumber: function(e){
+        if (game.selectedNumbers.length >= 89) {
+            console.log(game.selectedNumbers)
+            return;
+        }
+
+        do {
+            newNumber = Math.round(Math.random() * 100);
+        } while (!game.isNumberSelectable(newNumber))
+        
+        game.selectedNumbers.push(newNumber);
+        document.querySelectorAll('.cell').forEach(function(cell){
+            console.log(newNumber);
+            if (cell.textContent === newNumber.toString()){
+                cell.classList.add('selected')
+            }
+        })
+    },
+    isNumberSelectable: function(num){
+        return num <= 89 
+            && num > 0
+            && !game.selectedNumbers.includes(num);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', game.init);
